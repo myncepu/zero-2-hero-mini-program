a7d5d27bb8423293635f5c685f5996b96364df60
Yan Pengqiang <yan2010@live.com>
2019-11-10 11:34:12 +0800

    Add Swiper to page Index

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,9 +1,13 @@
 import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { Swiper, SwiperItem, View, Image } from '@tarojs/components'
 import './index.scss'
 
-export default class Index extends Component {
+type IndexProps = {}
 
+type IndexState = {
+  imgUrls: string[]
+}
+export default class Index extends Component<IndexProps, IndexState> {
   /**
    * 指定config的类型声明为: Taro.Config
    *
@@ -12,23 +16,50 @@ export default class Index extends Component {
    * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
    */
   config: Config = {
-    navigationBarTitleText: '首页'
+    navigationBarTitleText: 'HEY',
   }
 
-  componentWillMount () { }
+  constructor() {
+    super(...arguments)
+    this.state = {
+      imgUrls: [
+        'https://img10.360buyimg.com/babel/s700x360_jfs/t25855/203/725883724/96703/5a598a0f/5b7a22e1Nfd6ba344.jpg!q90!cc_350x180',
+        'https://img11.360buyimg.com/babel/s700x360_jfs/t1/4776/39/2280/143162/5b9642a5E83bcda10/d93064343eb12276.jpg!q90!cc_350x180',
+        'https://img14.360buyimg.com/babel/s700x360_jfs/t1/4099/12/2578/101668/5b971b4bE65ae279d/89dd1764797acfd9.jpg!q90!cc_350x180',
+      ],
+    }
+  }
 
-  componentDidMount () { }
+  componentWillMount() {}
 
-  componentWillUnmount () { }
+  componentDidMount() {}
 
-  componentDidShow () { }
+  componentWillUnmount() {}
 
-  componentDidHide () { }
+  componentDidShow() {}
 
-  render () {
+  componentDidHide() {}
+
+  render() {
+    const { imgUrls } = this.state
+
     return (
-      <View className='index'>
-        <Text>Hello world!</Text>
+      <View className='page'>
+        <View className='example-item'>
+          <Swiper
+            indicatorColor='#999'
+            indicatorActiveColor='#333'
+            circular={true}
+            autoplay={true}
+            indicatorDots={true}
+          >
+            {imgUrls.map((item, idx) => (
+              <SwiperItem key={idx}>
+                <Image src={item} className='slide-image' />
+              </SwiperItem>
+            ))}
+          </Swiper>
+        </View>
       </View>
     )
   }
