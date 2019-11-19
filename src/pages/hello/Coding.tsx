34bbf7110ad89cccdf684cc808ca4fe2d0b2a2f5
Yan Pengqiang <yan2010@live.com>
2019-11-20 00:17:50 +0800

    send params to qrcode page while navigating

--- src/pages/hello/Coding.tsx
+++ src/pages/hello/Coding.tsx
@@ -1,6 +1,7 @@
 import { View, Image, Text } from "@tarojs/components";
 import Taro, { Component } from "@tarojs/taro";
 import _ from "lodash";
+import QueryString from "query-string";
 import "./Coding.scss";
 
 type ProjectsProps = {
@@ -9,28 +10,41 @@ type ProjectsProps = {
     description: string;
     type: string;
     cover: string;
+    href: string;
   }[];
 };
 
 export default class extends Component<ProjectsProps> {
+  navigateTo(props) {
+    const url = `/pages/qrcode/index?${props}`;
+    Taro.navigateTo({ url });
+  }
+
   render() {
     const { data } = this.props;
     return (
       <View className="coding">
-        {data.map((item, idx) => (
-          <View className="box" key={idx} hoverClass="box-hover">
-            <Image
-              src={item.cover}
-              style={{ width: "50px", height: "50px", borderRadius: "5px" }}
-              mode="aspectFill"
-            />
-            <View className="text">
-              <Text className="title">{item.title}</Text>
-              <Text className="description">{item.description}</Text>
-              <Text className="type">{item.type}</Text>
+        {data.map((item, idx) => {
+          const url = QueryString.stringify({
+            url: item.href,
+            title: item.title
+          });
+          return (
+            <View
+              className="box"
+              key={idx}
+              hoverClass="box-hover"
+              onClick={this.navigateTo.bind(this, url)}
+            >
+              <Image src={item.cover} mode="aspectFill" className="image" />
+              <View className="text">
+                <Text className="title">{item.title}</Text>
+                <Text className="description">{item.description}</Text>
+                <Text className="type">{item.type}</Text>
+              </View>
             </View>
-          </View>
-        ))}
+          );
+        })}
       </View>
     );
   }
